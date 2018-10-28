6257ddf006518d581fd75eb68a818825269adef1
fengchaolin <linfengchao@taqu.cn>
2018-10-28 22:47:55 +0800

    头部视差效果，有时候会出现留白，待解决

--- app/src/main/java/com/mrchao/ui/headparallx/HeadParallxBehavior.java
+++ app/src/main/java/com/mrchao/ui/headparallx/HeadParallxBehavior.java
@@ -6,37 +6,41 @@ import android.content.Context;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.view.ViewCompat;
-import android.support.v7.widget.Toolbar;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 
 
 /**
- * Created by gjm on 2017/5/24.
- * 目前包括的事件：
- * 图片放大回弹
- * 个人信息布局的top和botoom跟随图片位移
- * toolbar背景变色
+ * Created by Linfc on 2018/10/27.
+ * <p>
+ * 图片放大回弹,背景变色
  */
-public class AppBarLayoutOverScrollViewBehavior extends AppBarLayout.Behavior {
+public class HeadParallxBehavior extends AppBarLayout.Behavior {
     private static final String TAG = "overScroll";
-    private static final String TAG_TOOLBAR = "toolbar";
-    private static final String TAG_MIDDLE = "middle";
     private static final float TARGET_HEIGHT = 1500;
     private View mTargetView;
     private int mParentHeight;
     private int mTargetViewHeight;
+    /**
+     * 总共偏移量
+     */
     private float mTotalDy;
     private float mLastScale;
     private int mLastBottom;
     private boolean isAnimate;
-    private final float MAX_REFRESH_LIMIT = 0.3f;//达到这个下拉临界值就开始刷新动画
+    /**
+     * 是否正在回弹中
+     */
+    private boolean isRecovering = false;
 
-    public AppBarLayoutOverScrollViewBehavior() {
+    public HeadParallxBehavior() {
     }
 
-    public AppBarLayoutOverScrollViewBehavior(Context context, AttributeSet attrs) {
+    public HeadParallxBehavior(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
@@ -44,7 +48,6 @@ public class AppBarLayoutOverScrollViewBehavior extends AppBarLayout.Behavior {
     @Override
     public boolean onLayoutChild(CoordinatorLayout parent, AppBarLayout abl, int layoutDirection) {
         boolean handled = super.onLayoutChild(parent, abl, layoutDirection);
-
         // 需要在调用过super.onLayoutChild()方法之后获取
         if (mTargetView == null) {
             mTargetView = parent.findViewWithTag(TAG);
@@ -52,43 +55,43 @@ public class AppBarLayoutOverScrollViewBehavior extends AppBarLayout.Behavior {
                 initial(abl);
             }
         }
-
         return handled;
     }
 
     private void initial(AppBarLayout abl) {
+        //设置不裁剪子view，一定要设置否则无法出现视差效果
         abl.setClipChildren(false);
         mParentHeight = abl.getHeight();
         mTargetViewHeight = mTargetView.getHeight();
     }
 
-
     @Override
     public boolean onStartNestedScroll(CoordinatorLayout parent, AppBarLayout child, View directTargetChild, View target, int nestedScrollAxes) {
         isAnimate = true;
+        //保证拉动头部时/和Tablayout 也能出现视差效果
+        if (target instanceof DisInterceptNestedScrollView) return true;
         return super.onStartNestedScroll(parent, child, directTargetChild, target, nestedScrollAxes);
     }
 
     @Override
     public void onNestedPreScroll(CoordinatorLayout coordinatorLayout, AppBarLayout child, View target, int dx, int dy, int[] consumed) {
-        if (mTargetView != null && ((dy < 0 && child.getBottom() >= mParentHeight)
-                || (dy > 0 && child.getBottom() > mParentHeight))) {
-            scale(child, target, dy);
-        } else {
-            super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed);
-
+        //当达到临界值开始出现视差效果
+        if (!isRecovering) {
+            if (mTargetView != null && ((dy < 0 && child.getBottom() >= mParentHeight) || (dy > 0 && child.getBottom() > mParentHeight))) {
+                scale(child, target, dy,consumed);
+                return;
+            }
         }
-
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed);
     }
 
     @Override
     public boolean onNestedPreFling(CoordinatorLayout coordinatorLayout, AppBarLayout child, View target, float velocityX, float velocityY) {
-        if (velocityY > 100) {//当y速度>100,就秒弹回
+        if (velocityY > 500) {//当y速度>100,就秒弹回
             isAnimate = false;
         }
         return super.onNestedPreFling(coordinatorLayout, child, target, velocityX, velocityY);
     }
-
 
     @Override
     public void onStopNestedScroll(CoordinatorLayout coordinatorLayout, AppBarLayout abl, View target) {
@@ -96,36 +99,38 @@ public class AppBarLayoutOverScrollViewBehavior extends AppBarLayout.Behavior {
         super.onStopNestedScroll(coordinatorLayout, abl, target);
     }
 
-
-    private void scale(AppBarLayout abl, View target, int dy) {
+    private void scale(AppBarLayout abl, View target, int dy,int[] consumed) {
         mTotalDy += -dy;
         mTotalDy = Math.min(mTotalDy, TARGET_HEIGHT);
         mLastScale = Math.max(1f, 1f + mTotalDy / TARGET_HEIGHT);
         ViewCompat.setScaleX(mTargetView, mLastScale);
         ViewCompat.setScaleY(mTargetView, mLastScale);
-        mLastBottom = mParentHeight + (int) (mTargetViewHeight / 2 * (mLastScale - 1));
+        mLastBottom = mParentHeight + (int) (mTargetViewHeight/2  * (mLastScale - 1));
         abl.setBottom(mLastBottom);
         target.setScrollY(0);
+        //滑动了多少距离=mTotalDy  子view滑动了多少距离offset=(int) (mTargetViewHeight / 2 * (mLastScale - 1))，
+        //需要把这个自 view滑动了多少距离赋值给consumed[1] 来告诉coordinatorlayout这样他就知道还只有处理这剩下的mTotalDy-offset 距离
+        //如果不加这个，当头部处于拉伸状态，手指不抬起，底下recycleView会跟着滑动一段距离，appbarlayout才会折叠退出
+        consumed[1] = (int) (mTargetViewHeight /2 * (mLastScale - 1));//
     }
 
-
-
     private void recovery(final AppBarLayout abl) {
+        if (isRecovering) return;
         if (mTotalDy > 0) {
+            isRecovering = true;
             mTotalDy = 0;
             if (isAnimate) {
                 ValueAnimator anim = ValueAnimator.ofFloat(mLastScale, 1f).setDuration(200);
-                anim.addUpdateListener(
-                        new ValueAnimator.AnimatorUpdateListener() {
-                            @Override
-                            public void onAnimationUpdate(ValueAnimator animation) {
-
-                                float value = (float) animation.getAnimatedValue();
-                                ViewCompat.setScaleX(mTargetView, value);
-                                ViewCompat.setScaleY(mTargetView, value);
-                                abl.setBottom((int) (mLastBottom - (mLastBottom - mParentHeight) * animation.getAnimatedFraction()));
-                            }
+                anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                    @Override
+                    public void onAnimationUpdate(ValueAnimator animation) {
+                            float value = (float) animation.getAnimatedValue();
+                            ViewCompat.setScaleX(mTargetView, value);
+                            ViewCompat.setScaleY(mTargetView, value);
+                            int bottomOffest = (int) (mLastBottom - (mLastBottom - mParentHeight) * animation.getAnimatedFraction());
+                            abl.setBottom(bottomOffest);
                         }
+                    }
                 );
                 anim.addListener(new Animator.AnimatorListener() {
                     @Override
@@ -134,6 +139,7 @@ public class AppBarLayoutOverScrollViewBehavior extends AppBarLayout.Behavior {
 
                     @Override
                     public void onAnimationEnd(Animator animation) {
+                        isRecovering = false;
                     }
 
                     @Override
@@ -149,6 +155,7 @@ public class AppBarLayoutOverScrollViewBehavior extends AppBarLayout.Behavior {
                 ViewCompat.setScaleX(mTargetView, 1f);
                 ViewCompat.setScaleY(mTargetView, 1f);
                 abl.setBottom(mParentHeight);
+                isRecovering = false;
             }
         }
     }
--- app/src/main/java/com/mrchao/ui/headparallx/HeadPeaxallActivity.java
+++ app/src/main/java/com/mrchao/ui/headparallx/HeadPeaxallActivity.java
@@ -1,17 +1,27 @@
 package com.mrchao.ui.headparallx;
 
+import android.content.Context;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.LinearLayout;
 
+import com.jaeger.library.StatusBarUtil;
 import com.mrchao.ui.R;
 import com.mrchao.ui.base.BaseActivity;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by MrChao on 2018/2/10.
@@ -26,22 +36,71 @@ public class HeadPeaxallActivity extends BaseActivity {
     @BindView(R.id.tl_view)
     TabLayout mTlView;
     @BindView(R.id.vp_view)
-    ViewPager mVpView;
+    NoScrollViewPager mVpView;
+    @BindView(R.id.llTopTitle)
+    LinearLayout llTopTitle;
+    @BindView(R.id.appbarlayout)
+    AppBarLayout appbarlayout;
+    @BindView(R.id.llcentent)
+    LinearLayout llcentent;
+    @BindView(R.id.root)
+    CoordinatorLayout root;
+    @BindView(R.id.toobar)
+    Toolbar toobar;
     private String[] title = {"主页", "微博", "相册"};
 
     @Override
     public int getLayoutId() {
         return R.layout.ac_head_parallax;
+
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void init() {
+        StatusBarUtil.setTranslucentForImageView(this, null);
         MyPageAdapter adapter = new MyPageAdapter(getSupportFragmentManager());
         mVpView.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTlView));
         mTlView.setupWithViewPager(mVpView);
         mVpView.setAdapter(adapter);
-        mVpView.setNestedScrollingEnabled(false);
+//        mVpView.setNestedScrollingEnabled(false);
+        int statuBarHeight = getStatuBarHeight(mContext);
+        CollapsingToolbarLayout.LayoutParams layoutParams = (CollapsingToolbarLayout.LayoutParams) llTopTitle.getLayoutParams();
+        layoutParams.topMargin = statuBarHeight;
+        llTopTitle.setLayoutParams(layoutParams);
+        //利用toobar属性来占位，使得自定义的headBar能够停在头部
+        CollapsingToolbarLayout.LayoutParams layoutParams2 = (CollapsingToolbarLayout.LayoutParams) toobar.getLayoutParams();
+        layoutParams2.topMargin = statuBarHeight;
+        toobar.setLayoutParams(layoutParams2);
+
+
+        appbarlayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                float percent = Float.valueOf(Math.abs(verticalOffset)) / Float.valueOf(appbarlayout.getTotalScrollRange());
+                Log.e("=======>",percent+"");
+                llTopTitle.setAlpha(percent);//0-1
+                StatusBarUtil.setTranslucentForImageView(HeadPeaxallActivity.this, (int) (255 * percent), null);
+                if (percent == 0) {//表示还能用滚动
+                    mVpView.setNoScroll(false);
+
+                } else if (percent == 1){//表示已经完全展开
+                    mVpView.setNoScroll(false);
+
+                } else {
+                    mVpView.setNoScroll(true);
+                }
+            }
+        });
+
+
+    }
+
+    public static int getStatuBarHeight(Context context) {
+        int identifier = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        return context.getResources().getDimensionPixelSize(identifier);
+
+
     }
 
 
